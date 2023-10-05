e5d20b105288a7f21d87bfbb736e254778ec0bd5
Mikita Shidlouski <retdarkw@gmail.com>
2023-10-05 14:29:12 +0300

    Change some rows for task 4(they were redundant)

--- src/lab1/task4/Task4.java
+++ src/lab1/task4/Task4.java
@@ -1,19 +1,9 @@
 package lab1.task4;
 
-import java.util.Scanner;
-
 public class Task4 {
 
     public static void main(String[] args) {
-//        int[] values = new int[] {1, 2, 5, 9, 10, 13};
-//        System.out.println(Logic.primePositionsString(Logic.numsOfPos(values)));
-
-        Scanner scan = new Scanner(System.in);
-        System.out.println("test");
-        int amount;
-
-        amount = Integer.parseInt(scan.nextLine());
-
-        System.out.println("Any");
+        int[] values = new int[] {1, 2, 5, 9, 10, 13};
+        System.out.println(Logic.primePositionsString(Logic.numsOfPos(values)));
     }
 }
